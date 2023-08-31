a7c9c427412c574dc288a56604db4a9d03171277
Johan Mosquera <joncamilomoni@gmail.com>
2023-08-31 12:02:09 -0500

    Registro de datos

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional, List
 from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
 from config.database import Session, engine, Base
-from models.movie import Movie
+from models.movie import Movie as MovieModel
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
@@ -28,7 +28,7 @@ class Movie(BaseModel):
     id: Optional[int] = None
     title: str = Field(min_length=5, max_length=15)
     overview: str = Field(min_length=15, max_length=50)
-    year: int = Field(le=2022)
+    year: int = Field(le=2023)
     rating:float = Field(ge=1, le=10)
     category:str = Field(min_length=5, max_length=15)
 
@@ -92,7 +92,10 @@ def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -
 
 @app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
 def create_movie(movie: Movie) -> dict:
-    movies.append(movie)
+    db = Session()
+    new_movie = MovieModel(**movie.model_dump())
+    db.add(new_movie)
+    db.commit()
     return JSONResponse(status_code=201, content={"message": "Se ha registrado la película"})
 
 @app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
